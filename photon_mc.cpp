0b4e1310dbac9218a1d38a013c613145a1e3fe84
John Angelis <john_agelis@hotmail.com>
2019-04-20 17:10:23 +0300

    Fixing counter

--- photon_mc.cpp
+++ photon_mc.cpp
@@ -4,17 +4,21 @@ g++ photon_mc.cpp -o photon_mc.exe `root-config --cflags --glibs`
 #include <iostream>
 #include <vector>
 #include <ROOT/RDataFrame.hxx>
-#include <ROOT/TRandom3.h>
-#include <ROOT/TMath.h>
+#include <TRandom3.h>
+#include <TMath.h>
+#include <TCanvas.h>
+#include <TApplication.h>
 
 using namespace std;
 using namespace ROOT;
 
-int main()
+int main(int argc, char **argv)
 {
+    TApplication theApp("App", &argc, argv);
     auto treename = "myTree";
     auto filename = "photon_tracks.root";
     constexpr int N = 10;
+    constexpr double Radius = 1.0;
     constexpr double beta0 = TMath::DegToRad() * 60.0;
     TRandom3 R(1);
     RDataFrame photons(N);
@@ -26,14 +30,34 @@ int main()
         return TMath::ACos(R.Uniform(1.0));
     };
 
-    photons.Define("x", genXY).Define("y", genXY).Snapshot(treename, filename);
+    auto genPhi = [&]() {
+        return fmod((R.Uniform(1.0) * 2.0 * M_PI), M_PI);
+    };
 
-    auto cuts = photons.Filter("x*x+y*y<=1").Define("phi", [&]() {
-                                                            return R.Uniform(1.0) * 2.0 * M_PI;
-                                                        })
-                    .Define("alpha", "asin((x*x+y*y)*sin(phi))")
-                    .Define("theta", genTheta)
-                    .Define("beta", "acos(sin(theta)*cos(alpha))")
-                    .Filter([](double b) { return b > beta0; }, {"beta"});
+    photons.Define("x", genXY).Define("y", genXY).Define("r", "x*x+y*y").Define("phi", genPhi).Define("alpha", "asin(r*r*sin(phi))").Define("theta", genTheta).Define("beta", "acos(sin(theta)*cos(alpha))").Snapshot(treename, filename);
+
+    RDataFrame photons_load(treename, filename);
+
+    // auto augmented = photons_load.Define("r", "x*x+y*y")
+    //                      .Define("phi", genPhi)
+    //                      .Define("alpha", "asin((x*x+y*y)*sin(phi))")
+    //                      .Define("theta", genTheta)
+    //                      .Define("beta", "acos(sin(theta)*cos(alpha))");
+    auto filter1 = photons_load.Filter("r<=1.0", "Radius").Filter([&](const double &b) { return b > beta0; }, {"beta"}, "beta_cut");
+    // auto filter2 = filter1.Filter([&](const double &b) { return b > beta0; }, {"beta"}, "beta_cut");
+    auto c2 = new TCanvas("c2", "test", 100, 100, 800, 600);
+    auto myHist = filter1.Histo1D("y");
+    myHist->DrawClone();
+    c2->Update();
+    auto counter1 = filter1.Count();
+    // auto counter22 = filter1.Count();
+    cout << *counter1 << endl;
+    // cout << *counter22 << endl;
+    auto d1 = filter1.Display("", 35);
+    d1->Print();
+    cout << "stats:" << endl;
+    auto report = photons_load.Report();
+    report->Print();
+    theApp.Run();
     return 0;
 }
