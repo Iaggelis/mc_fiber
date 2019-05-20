fe8452e76ec97320e8c85aa02c6db161af63a2d2
Iaggelis <john_agelis@hotmail.com>
2019-05-20 11:54:01 +0300

    trying different theta distributions

--- photon2.cpp
+++ photon2.cpp
@@ -1,22 +1,28 @@
 /* compile with:
-g++ photon_mc.cpp -o photon_mc.exe `root-config --cflags --glibs`
+g++ photon2.cpp -o photon2.exe `root-config --cflags --glibs`
 */
 #include <iostream>
 #include <vector>
 #include <ROOT/RDataFrame.hxx>
 #include <TRandom3.h>
 #include <TMath.h>
+#include <TF1.h>
 #include <TCanvas.h>
 #include <TApplication.h>
 
 using namespace std;
 using namespace ROOT;
 TRandom3 R(1);
+auto f1 = new TF1("f1", "sin(x)", 0, M_PI_2);
+
 auto genXY = [&]() {
     return R.Uniform(1.0);
 };
 auto genTheta = [&]() {
-    return TMath::ACos(R.Uniform(1.0));
+    // return TMath::ACos(R.Uniform(1.0));
+    // return TMath::ASin(sqrt(R.Uniform(1.0)));
+    // return TMath::ACos(TMath::Sqrt(1.0 - R.Uniform(1.0)));
+    return f1->GetRandom();
 };
 
 auto genPhi = [&]() {
@@ -37,14 +43,14 @@ int main(int argc, char **argv)
     TApplication theApp("App", &argc, argv);
     auto treename = "myTree";
     auto filename = "photon_tracks.root";
-    constexpr long N = 100000;
+    constexpr long N = 1000000;
     constexpr double Radius = 1.0;
     constexpr double beta0 = TMath::DegToRad() * 60.0;
     fill_tree(treename, filename, N);
 
     RDataFrame photons_load(treename, filename);
     auto augmented = photons_load.Define("lenght", "x+y");
-    auto filter1 = photons_load.Filter("x*x+y*y<=1.0", "Radius").Define("phi", genPhi).Define("alpha", "asin((x*x+y*y)*sin(phi))").Define("theta", genTheta).Define("beta", "acos(sin(theta)*cos(alpha))").Filter([&](const double &b) { return b > beta0; }, {"beta"}, "beta_cut");
+    auto filter1 = photons_load.Define("r", "x*x+y*y").Filter("r*r<=1.0", "Radius").Define("phi", genPhi).Define("alpha", "asin(r*sin(phi))").Define("theta", genTheta).Define("beta", "acos(sin(theta)*cos(alpha))").Filter([&](const double &b) { return b >= beta0; }, {"beta"}, "beta_cut");
     auto counter1 = filter1.Count();
     cout << *counter1 << endl;
 
@@ -52,7 +58,7 @@ int main(int argc, char **argv)
 
     auto trackHist = augmented.Graph("x", "lenght");
     auto length = augmented.Range(0, 10);
-    auto myHist = filter1.Histo1D({"myHist", "photon yield", 10, 0, 1}, "x");
+    auto myHist = filter1.Histo1D({"myHist", "photon yield", 20, 0, 1}, "x");
     myHist->Scale(1.0 / N);
     myHist->Draw("L HIST");
     // c2->Update();
